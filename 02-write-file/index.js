a35d9400003a320b53d2086210a0b393b94afd2b
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2025-01-23 00:05:19 +0300

    fix: add greetings, and farewell when pressing the ctrl + c

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,14 +2,15 @@ const { createWriteStream } = require('fs');
 const path = require('path');
 const { stdout, stdin, exit } = process;
 
+console.log('Greetings! Please, enter some text...');
+
 const file = path.join(__dirname, 'text.txt');
 const output = createWriteStream(file);
 const setBuy = () => stdout.write('Goodbye!');
 
-stdin.on('start', () => stdout.write('Greetings! Please, enter some text...'));
 stdin.on('data', (data) => {
   if (data.toString().trim() === 'exit') exit();
   output.write(data);
 });
-stdin.on('end', () => setBuy());
+process.on('SIGINT', () => exit());
 process.on('exit', () => setBuy());
